ea28dc0e606c967597f8b1d8475e2771a894f0d8
aunne <unihae236@gmail.com>
2023-01-25 21:28:03 +0800

    update

--- front/js/Request.js
+++ front/js/Request.js
@@ -1,6 +1,6 @@
 export default function Request() {
     const req = axios.create({
-        baseURL: 'http://localhost/bet539/backend',
+        baseURL: 'https://b48a-125-229-161-9.jp.ngrok.io//bet539/backend',
         headers: { 'Authorization': window.localStorage.getItem("jwtToken") }
     })
     return req;
