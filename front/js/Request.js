322991d1a1e5da611bd494528c759166116538c4
Aunne <unihae236@example.com>
2023-01-25 21:34:53 +0800

    delete git Request.js

--- front/js/Request.js
+++
@@ -1,7 +0,0 @@
-export default function Request() {
-    const req = axios.create({
-        baseURL: 'https://b48a-125-229-161-9.jp.ngrok.io//bet539/backend',
-        headers: { 'Authorization': window.localStorage.getItem("jwtToken") }
-    })
-    return req;
-}
