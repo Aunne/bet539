053e8cda8128d9826678e3ce781f58b762eba4cc
aunne <unihae236@gmail.com>
2023-02-09 22:44:03 +0800

    Adjustment rule img css

--- front/js/menu/displayRule.js
+++ front/js/menu/displayRule.js
@@ -1,6 +1,6 @@
 export default function () {
     let str = `
-        <img src="./img/bet539_rule.png">
+        <img src="./img/bet539_rule.png" style="width:70%;heigh:100%">
     `;
 
     $("#content").html(str);
